1d2be35e4becf9a27d65a2890260a67419344f7d
MF Nowlan <fitznowlan@gmail.com>
2016-04-03 13:23:42 -0400

    Added DQT used by Java

--- src/main/scala/com/root81/atrium/core/JPEGQuantization.scala
+++ src/main/scala/com/root81/atrium/core/JPEGQuantization.scala
@@ -10,15 +10,26 @@ object JPEGQuantization {
 
   private val DIMENSION = 8
 
-  val JPEG_QUANTIZERS_50 = Vector(
-    Vector( 16,  11,  10,  16,  24,  40,  51,  61),
-    Vector( 12,  12,  14,  19,  26,  58,  60,  55),
-    Vector( 14,  13,  16,  24,  40,  57,  69,  56),
-    Vector( 14,  17,  22,  29,  51,  87,  80,  62),
-    Vector( 18,  22,  37,  56,  68, 109, 103,  77),
-    Vector( 24,  35,  55,  64,  81, 104, 113,  92),
-    Vector( 49,  64,  78,  87, 103, 121, 120, 101),
-    Vector( 72,  92,  95,  98, 112, 100, 103,  99)
+  val JPEG_SPEC_QUANTIZERS_50 = Vector(
+    Vector(  16,  11,  10,  16,  24,  40,  51,  61),
+    Vector(  12,  12,  14,  19,  26,  58,  60,  55),
+    Vector(  14,  13,  16,  24,  40,  57,  69,  56),
+    Vector(  14,  17,  22,  29,  51,  87,  80,  62),
+    Vector(  18,  22,  37,  56,  68, 109, 103,  77),
+    Vector(  24,  35,  55,  64,  81, 104, 113,  92),
+    Vector(  49,  64,  78,  87, 103, 121, 120, 101),
+    Vector(  72,  92,  95,  98, 112, 100, 103,  99)
+  )
+
+  val JAVA_QUANTIZERS_50 = Vector(
+    Vector(  16,  11,  12,  14,  12,  10,  16,  14),
+    Vector(  13,  14,  18,  17,  16,  19,  24,  40),
+    Vector(  26,  24,  22,  22,  24,  49,  35,  37),
+    Vector(  29,  40,  58,  51,  61,  60,  57,  51),
+    Vector(  56,  55,  64,  72,  92,  78,  64,  68),
+    Vector(  87,  69,  55,  56,  80, 109,  81,  87),
+    Vector(  95,  98, 103, 104, 103,  62,  77, 113),
+    Vector( 121, 112, 100, 120,  92, 101, 103,  99)
   )
 
   def quantize(channel: Vector[Vector[Double]], quality: Int): QuantizedMatrix = {
@@ -26,7 +37,7 @@ object JPEGQuantization {
 
     val scaleFactor = getScaleFactor(quality)
 
-    val coefficients = channel.zip(JPEG_QUANTIZERS_50).map {
+    val coefficients = channel.zip(JAVA_QUANTIZERS_50).map {
       case (dctRow, quantRow) => dctRow.zip(quantRow).map {
         case (coef, quant) => (coef / (quant * scaleFactor)).round.toInt
       }
@@ -43,7 +54,7 @@ object JPEGQuantization {
 
     val scaleFactor = getScaleFactor(matrix.quality)
 
-    matrix.coefficients.zip(JPEG_QUANTIZERS_50).map {
+    matrix.coefficients.zip(JAVA_QUANTIZERS_50).map {
       case (coefRow, quantRow) => coefRow.zip(quantRow).map {
         case (coef, quant) => coef * quant * scaleFactor
       }
