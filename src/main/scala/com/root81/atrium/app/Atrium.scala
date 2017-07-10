ee735a3b757f556d5b106eb6bf784b0c04d33cff
MF Nowlan <fitznowlan@gmail.com>
2017-07-10 00:02:11 -0400

    Make quality argument optional

--- src/main/scala/com/root81/atrium/app/Atrium.scala
+++ src/main/scala/com/root81/atrium/app/Atrium.scala
@@ -10,12 +10,37 @@ import com.root81.atrium.core._
 import com.root81.atrium.utils._
 import java.io.File
 import javax.imageio.ImageIO
+import scala.util.Try
+
+//
+// Application argument types
+//
 
 sealed trait AtriumArgs
-case class DecodeArgs(path: String, quality: Int) extends AtriumArgs
-case class EncodeArgs(inPath: String, quality: Int, message: String, outPath: Option[String] = None) extends AtriumArgs
-case class InfoArgs(path: String) extends AtriumArgs
-case class ExitArgs(code: Int, errMsg: Option[String] = None) extends AtriumArgs
+
+case class DecodeArgs(
+  path: String
+) extends AtriumArgs
+
+case class EncodeArgs(
+  inPath: String,
+  message: String,
+  outPath: Option[String] = None,
+  quality: Option[Int] = None
+) extends AtriumArgs
+
+case class InfoArgs(
+  path: String
+) extends AtriumArgs
+
+case class ExitArgs(
+  code: Int,
+  errMsg: Option[String] = None
+) extends AtriumArgs
+
+//
+// Application
+//
 
 object Atrium {
   import AtriumCore._
@@ -24,7 +49,9 @@ object Atrium {
   private val COMMAND_DECODE = "decode"
   private val COMMAND_INFO = "info"
 
-  private val OPT_ENCODE_OUTPUT = "--out"
+  private val OPTION_PREFIX = "--"
+  private val OPT_ENCODE_OUTPUT = OPTION_PREFIX + "out"
+  private val OPT_ENCODE_QUALITY = OPTION_PREFIX + "quality"
 
   private val OPT_HELP = "--help"
   private val OPTS_HELP = Set("help", OPT_HELP)
@@ -58,19 +85,22 @@ object Atrium {
     AtriumLogger.debug("Atrium: ENCODE")
 
     val inputImage = ImageLoader.loadJPGImage(args.inPath)
+    val inputImageInfo = getJPEGInfoForImagePath(args.inPath)
+
     val regionedImage = getRegionedImage(inputImage)
+    val quality = args.quality.getOrElse(inputImageInfo.quality)
 
     AtriumLogger.debug(s"Atrium: $regionedImage")
     AtriumLogger.debug(s"Atrium: input message ${args.message.length} characters - '${args.message}'")
 
-    val regionsWithEncodedBytes = encodeMessageIntoRegions(regionedImage.regions, args.message, args.quality)
+    val regionsWithEncodedBytes = encodeMessageIntoRegions(regionedImage.regions, args.message, quality)
 
     // Write out the encoded regions into a new image.
     val regionedImageWithBytes = regionedImage.copy(regions = regionsWithEncodedBytes)
     val outputImage = ImageConversions.toBufferedImage(regionedImageWithBytes)
     val outputPath = args.outPath.getOrElse(getDefaultOutputPathFromInputPath(args.inPath))
 
-    ImageLoader.writeImageToJPGFile(outputPath, outputImage, args.quality)
+    ImageLoader.writeImageToJPGFile(outputPath, outputImage, quality)
 
     AtriumLogger.debug(s"Atrium: Encoded image written to: $outputPath")
   }
@@ -79,11 +109,13 @@ object Atrium {
     AtriumLogger.debug("Atrium: DECODE")
 
     val inputImage = ImageLoader.loadJPGImage(args.path)
+    val inputImageInfo = getJPEGInfoForImagePath(args.path)
+
     val regionedImage = getRegionedImage(inputImage)
 
     AtriumLogger.debug(s"Atrium: $regionedImage")
 
-    val message = decodeMessageFromRegions(regionedImage.regions, args.quality)
+    val message = decodeMessageFromRegions(regionedImage.regions, inputImageInfo.quality)
 
     AtriumLogger.debug(s"Atrium: decoded ${message.length} message characters")
 
@@ -121,16 +153,33 @@ object Atrium {
 
     args.headOption match {
       case Some(COMMAND_ENCODE) => {
-        args.drop(1).toList match {
-          case OPT_ENCODE_OUTPUT :: outputPath :: inputPath :: quality :: message :: Nil => EncodeArgs(inputPath, quality.toInt, message, Some(outputPath))
-          case inputPath :: quality :: message :: Nil => EncodeArgs(inputPath, quality.toInt, message)
-          case _ => ExitArgs(1, Some(s"atrium: 'encode' requires a jpg image path, a quality on [0, 100], and a message."))
+        val encodeArgs = args.drop(1).toList
+
+        val atriumArgs = argOptionParser(encodeArgs) match {
+          case Left(exitArgs) => exitArgs
+          case Right((remainingArgs, optionMap)) => {
+            // Requires an input path and a message.
+
+            if (remainingArgs.size < 2) {
+              ExitArgs(1, Some(s"atrium: 'encode' requires a jpg image path and a message."))
+
+            } else {
+              val (inputPath, message) = (remainingArgs.head, remainingArgs(1))
+
+              val outputPath = optionMap.get(OPT_ENCODE_OUTPUT)
+              val quality = optionMap.get(OPT_ENCODE_QUALITY).flatMap(v => Try(v.toInt).toOption)
+
+              EncodeArgs(inputPath, message, outputPath, quality)
+            }
+          }
         }
+
+        atriumArgs
       }
       case Some(COMMAND_DECODE) => {
         args.drop(1).toList match {
-          case path :: quality :: Nil => DecodeArgs(path, quality.toInt)
-          case _ => ExitArgs(1, Some(s"atrium: 'decode' requires a jpg image path and a quality on [0, 100]."))
+          case path :: Nil => DecodeArgs(path)
+          case _ => ExitArgs(1, Some(s"atrium: 'decode' requires a jpg image path."))
         }
       }
       case Some(COMMAND_INFO) => {
@@ -147,6 +196,26 @@ object Atrium {
         ExitArgs(0)
       }
     }
+  }
+
+  private def argOptionParser(
+    args: List[String],
+    options: Map[String, String] = Map.empty[String, String]
+  ): Either[ExitArgs, (List[String], Map[String, String])] = {
+
+    if (args.headOption.exists(_.startsWith(OPTION_PREFIX))) {
+      val option = args.head
+      if (args.tail.isEmpty) {
+        return Left(ExitArgs(1, Some(s"atrium: option '$option' requires a value")))
+      }
+
+      val optionValue = args.tail.head
+      val newOptions = options ++ Map(option -> optionValue)
+
+      return argOptionParser(args.tail.tail, newOptions)
+    }
+
+    Right(args, options)
   }
 
   private def exit(code: Int, message: Option[String] = None): Unit = {
